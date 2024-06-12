3ab6a51355c263b9641652f32eaf97fbe4cffbe8
PraveenHalhalli9 <33408383+PraveenHalhalli9@users.noreply.github.com>
2024-06-12 19:58:47 +0530

    Update ExpenseTrackerSteps.java

--- src/main/java/com/quantum/steps/ExpenseTrackerSteps.java
+++ src/main/java/com/quantum/steps/ExpenseTrackerSteps.java
@@ -13,6 +13,7 @@ public class ExpenseTrackerSteps {
 	@Then("I should see expense tracker login screen")
 	public void verifyExpenseTrackerLogin() {
 		new ExpenseTrackerLoginPage().verifyExpenseTrackerLoginScreen();
+	       System.out.println(ConfigurationManager.getBundle().getString(""));
 		/*PerfectoApplicationSteps.setSuccessSensorAuthenticationtoAppById(id);
 		PerfectoApplicationSteps.setSuccessFingerprinttoAppById(id);
 		PerfectoApplicationSteps.setFailSensorAuthenticationtoAppById(errorType, id);
